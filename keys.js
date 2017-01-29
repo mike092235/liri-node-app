a38fc2cca6ff9a8d11bbd06b45db63b25e971525
Michael Emmanuel <michael.emmanuel732@gmail.com>
2017-01-29 13:07:23 -0500

    installed npms

--- keys.js
+++ keys.js
@@ -1,4 +1,4 @@
-console.log('this is loaded');
+console.log('keys are loaded');
 
 exports.twitterKeys = {
   consumer_key: EUuJhU4JN2VQzmCcMIgSgobvT,
--- liri.js
+++ liri.js
@@ -1,2 +1,18 @@
-// Grab the request package
-var request = require('./keys.js');
+//----enable fs to read and write to .txt files----
+var fs = require("fs");
+
+//----enable twitter, spotify, request node packages----
+var twitter = require('twitter');
+var spotify = require('spotify');
+var request = require('request');
+
+
+//----store location of required file in variable----
+var keyFile = require('./keys.js');
+
+//----store location of twitter keys----
+var tKeys = keyFiles.twitterKeys;
+
+//----var for request----
+var userRequest;
+
